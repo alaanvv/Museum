ce53ce9a0e82787a9d70dca1ef147b0335b27f50
Alan Vale <valealan84@gmail.com>
2024-04-08 08:50:29 -0400

    feat: added my discord bot Say

---
+++ Say/index.js
@@ -0,0 +1,31 @@
+const { createAudioPlayer, createAudioResource, joinVoiceChannel } = require('@discordjs/voice')
+const { createReadStream } = require('fs')
+const { Client } = require('discord.js')
+const speak = require('simple-tts')
+
+const bot = new Client({ intents: 3276799 })
+
+bot.on('ready', bot => console.log(`Bot running as ${bot.user.tag}`))
+
+bot.on('messageCreate', async message => {
+  if (!message.content.startsWith('>') || !message.member.voice.channel) return
+
+  const player = createAudioPlayer()
+  joinVoiceChannel({
+    channelId: message.member.voice.channel.id,
+    guildId: message.channel.guildId,
+    adapterCreator: message.guild.voiceAdapterCreator
+  }).subscribe(player)
+
+  speak(
+    message.content.slice(1).trim().replaceAll(/[^a-zA-Z0-9 áÁàÀãÃéÉêÊíÍóÓôÔúÙçÇ]/g, ''), 
+    { 
+      format: 'mp3', filename: 'temp', lang: 'pt-br', 
+      pitch: 0, speed: 120, wordgap: 1 
+    }
+  )
+  await new Promise(resolve => setTimeout(resolve, 500))
+  player.play(createAudioResource(createReadStream('temp.mp3')))
+})
+
+bot.login('MTA3NzA0NDE0NzIzMzQ4ODk0Nw.G9ZKK-.Gf1vb4zPFs4YxWPpMVOuIbi9JlpgbtL-kTYCI0')
